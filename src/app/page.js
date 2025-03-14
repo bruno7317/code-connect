d27d02289737a262fa3c6ee58e883d66b2233026
Bruno de Souza da Silva <bruno7317@gmail.com>
2025-03-13 19:02:13 -0600

    Pagination

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,25 @@ import db from '../../prisma/db';
 
 async function getAllPosts(page) {
   try {
+
+    const perPage = 6;
+    const skip = (page - 1) * perPage;
+
+    const prev = page > 1 ? page - 1 : null
+
+    const totalPosts = await db.post.count()
+    const totalPages = Math.ceil( totalPosts / perPage );
+    const next = page < totalPages ? page + 1 : null
+
     const posts = await db.post.findMany({
+      take: perPage,
+      skip,
+      orderBy: { createdAt: 'desc' },
       include: {
         author: true
       }
     })
-    return { data: posts, prev: null, next: null }
+    return { data: posts, prev, next }
   } catch (error) {
     logger.error('Failure obtaining posts', { error })
     return { data: [], prev: null, next: null }
@@ -20,7 +33,7 @@ async function getAllPosts(page) {
 }
 
 export default async function Home({ searchParams }) {
-  const currentPage = searchParams?.page || 1
+  const currentPage = parseInt(searchParams?.page || 1)
   const { data: posts, prev, next } = await getAllPosts(currentPage)
   return (
     <main>
